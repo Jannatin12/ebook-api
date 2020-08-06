68d727d868c955cc3e930a44f21635f5542910c1
Jannatin Nurrohmah <jannatinnurrohmah@gmail.com>
2020-08-06 14:56:15 +0700

    Routing & Controller

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>NIS</td>
+            <td>:</td>
+            <td>3103118077</td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td>Jannatin Nurrohmah</td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td>:</td>
+            <td>Perempuan</td>
+        </tr>
+        <tr>
+            <td>No.Hp</td>
+            <td>:</td>
+            <td>085712869801</td>
+        </tr>
+        <tr>
+            <td>Kelas</td>
+            <td>:</td>
+            <td>XII RPL 3</td>
+        </tr>
+    </table>
+</body>
+</html>
